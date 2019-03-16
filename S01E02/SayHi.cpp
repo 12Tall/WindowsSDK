b70325a8aad57f7f66470fc88f3f2598de91942f
ouyounger <ouyounger@163.com>
2019-03-16 22:54:08 +0800

    TCHAR

--- S01E02/SayHi.cpp
+++ S01E02/SayHi.cpp
@@ -7,6 +7,7 @@ int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,PSTR szCmdLine,in
 	// 按钮风格：MB_OK，查看定义就好了
 	// 返回值：IDOK，查看定义就好了
 	// TEXT("") 主要是为了兼容Unicode
+	// 定义字符串可以用TCHAR  
 	MessageBox(NULL, TEXT("Hello World!"), TEXT("SayHi"), MB_OK|MB_ICONINFORMATION);
 
 	return 0;
