12a02c45c0e386912cc6621f1316463cc83e951e
ouyounger <ouyounger@163.com>
2019-03-16 22:24:48 +0800

    SayHi

---
+++ S01E02/SayHi.cpp
@@ -0,0 +1,10 @@
+﻿#include <Windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,PSTR szCmdLine,int iCmdShow)
+{
+	// 句柄，内容，标题，按钮风格
+	// 压栈顺序：自右向左(⚠)
+	MessageBox(NULL, TEXT("Hello World!"), TEXT("SayHi"), MB_OK);
+
+	return 0;
+}
