3dbe5026e29309dde962e574d27a9f2bd23daae5
ouyounger <ouyounger@163.com>
2019-03-16 22:47:44 +0800

    Unicode简要说明

--- S01E01/HelloWord.c
+++ S01E01/HelloWord.c
@@ -2,6 +2,8 @@
 
 int main(int argc, char* argv[])
 {
+	// 这里要兼容汉语，需要setlocale()
+	// 还需要宽字符wprintf()
 	printf("hello world!");
 	return 0;
 }
--- S01E02/SayHi.cpp
+++ S01E02/SayHi.cpp
@@ -2,9 +2,12 @@
 
 int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,PSTR szCmdLine,int iCmdShow)
 {
-	// 句柄，内容，标题，按钮风格
+	// 句柄，内容，标题，按钮风格(或运算)
 	// 压栈顺序：自右向左(⚠)
-	MessageBox(NULL, TEXT("Hello World!"), TEXT("SayHi"), MB_OK);
+	// 按钮风格：MB_OK，查看定义就好了
+	// 返回值：IDOK，查看定义就好了
+	// TEXT("") 主要是为了兼容Unicode
+	MessageBox(NULL, TEXT("Hello World!"), TEXT("SayHi"), MB_OK|MB_ICONINFORMATION);
 
 	return 0;
 }
